2c76f7c664ff5696eb7885f69891550cd3c0e6bf
Carlos Escribano <carlosescri@gmail.com>
2019-11-29 11:53:07 +0100

    fix: make it work in FF

--- dialog-remover.js
+++ dialog-remover.js
@@ -1,10 +1,30 @@
-function removeQuantcast() {
+const INTERVAL = 50;
+const MAX_TIME = 10000;
+
+function _remove(fn, limit) {
+  console.log(`trying to remove ${fn.name}…`);
+  if (fn()) {
+    console.log(`removed ${fn.name}!`);
+  } else if (limit >= 0) {
+    setTimeout(_remove, 75, fn, limit - 1);
+  }
+}
+
+async function remove(fn) {
+  _remove(fn, Math.floor(MAX_TIME / INTERVAL));
+}
+
+function quantcast() {
   const classNames = ['qc-cmp-ui-showing', 'qc-cmp-showing'];
-  document.querySelectorAll(classNames.map(c => `.${c}`).join(', ')).forEach(element => {
-    element.classList.remove(...classNames);
-  })
+  const elements = document.querySelectorAll(classNames.map(c => `.${c}`).join(', '));
+  if (elements.length === 0) {
+    return false;
+  } else {
+    elements.forEach(element => element.classList.remove(...classNames));
+    return true;
+  }
 }
 
 window.addEventListener('load', (e) => {
-  removeQuantcast();
+  remove(quantcast);
 });
